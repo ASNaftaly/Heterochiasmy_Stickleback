5a1e8a604c5ccf1c06f0ee1f0e3bc1dc953cb820
Alice <alice.shanfelter@gmail.com>
2020-10-19 08:43:32 -0400

    removed header lines

--- Hotspot_Analyses/BamtoBed_V5.sh
+++ Hotspot_Analyses/BamtoBed_V5.sh
@@ -1,14 +1,3 @@
-#PBS -S /bin/bash
-#PBS -q batch
-#PBS -N convert_bam_to_bed_V5
-#PBS -l nodes=1:ppn=1
-#PBS -l mem=15gb
-#PBS -l walltime=:00:00
-#PBS -M alice.shanfelter@uga.edu
-#PBS -m abe
-
-cd /scratch/afs16076/Paired_ATAC_RNA/ATAC_seq/Hotspot_comparisons/
-
 module load BEDTools/2.29.2-GCC-8.2.0-2.31.1
 
 #bedtools bamtobed -i LW_hotspots_V5_positions_sorted.bam >> LW_hotspots_V5_positions.bed
