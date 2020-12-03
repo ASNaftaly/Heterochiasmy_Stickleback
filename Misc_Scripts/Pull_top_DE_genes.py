3aea7f3c078a2362113f81cce77afd2c3291af97
Alice <alice.shanfelter@gmail.com>
2020-12-03 10:07:27 -0500

    Script to pull genes that are differentially expressed between ovary and testis

---
+++ Misc_Scripts/Pull_top_DE_genes.py
@@ -0,0 +1,65 @@
+#pulling top 20 upregulated testis genes compared to ovary samples
+#got normalized counts from DESeq2 in R
+#got adjusted p values and log2fold changes from DESeq2 in R
+#testis upregulated genes will have a negative log2fold change as these changes are in terms of ovary expression
+#normalized counts and adjusted pvalues are in csv files
+#to run script: python3 Pull_top_DE_genes.py <normalized read counts csv> <padj values in csv>
+
+import sys
+
+#read in normalized counts
+#returns dictionary with key == gene and value == [average ovary normalized read count, average testis normalized read count]
+def read_normalized_counts():
+    normalized_file = sys.argv[1]
+    normalized_counts_dict = {}
+    with open(normalized_file, 'r') as normalized_counts:
+        for line in normalized_counts:
+            new_line = line.split(",")
+            if new_line[0].startswith("Genes"):
+                continue
+            else:
+                gene = new_line[0].strip("\"")
+                #values in list are in the following order:
+                #L1F, L2F, L1M, L2M, O1, O2, T1, T2
+                #want average of Ovary and testis
+                ave_ovary_norm_read_counts = (float(new_line[5]) + float(new_line[6]))/2
+                ave_testis_norm_read_counts = (float(new_line[7]) + float(new_line[8].strip("\n")))/ 2
+                ave_norm_values = [ave_ovary_norm_read_counts, ave_testis_norm_read_counts]
+                normalized_counts_dict.update({gene:ave_norm_values})
+    return normalized_counts_dict
+
+#read in adjusted p values and log2fold values
+#returns dictionary where key == gene and value == [log2_fold, adjusted p value]
+def read_padj_values():
+    padj_file = sys.argv[2]
+    padj_dict = {}
+    with open(padj_file, 'r') as padj_vals:
+        for line in padj_vals:
+            new_line = line.split(",")
+            padj = ""
+            if new_line[0] == "genes":
+                continue
+            else:
+                gene = new_line[0].strip("\"")
+                log2_fold = float(new_line[1])
+                if new_line[2].strip("\n") == "NA":
+                    continue
+                else:
+                    padj = float(new_line[2].strip("\n"))
+                dict_value = [log2_fold, padj]
+                padj_dict.update({gene:dict_value})
+    return padj_dict
+
+
+#sorting padj dict to include the top genes with padj < 10e-6 and have negative log2_fold (meaning greater expression in the testis)
+def sort_padj():
+    padj_dict = read_padj_values()
+    significant_padj = {}
+    for gene in padj_dict:
+        single_gene = padj_dict[gene]
+        if single_gene[1] < 10e-6:
+            if single_gene[0] < 0:
+                print(single_gene)
+
+
+sort_padj()
