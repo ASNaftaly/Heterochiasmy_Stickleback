98d186e3b2b94f66f77ee0354b25581effd9783e
Alice Shanfelter Naftaly <31047990+ASNaftaly@users.noreply.github.com>
2021-12-29 07:51:25 -0800

    Add files via upload

---
+++ Motif_Analyses/MEME.example.sh
@@ -0,0 +1,4 @@
+
+ml MEME/5.0.0-iomkl-2018a
+
+meme -o <output directory name> -dna -revcomp -mod zoops -nmotifs 50 -p 12 -V -brief 11000 <peaks fasta>
---
+++ Motif_Analyses/Tomtom.example.sh
@@ -0,0 +1,4 @@
+
+ml MEME/5.0.0-iomkl-2018a
+
+tomtom -o <output directory for tomtom> <meme file from meme> JASPAR2018_CORE_vertebrates_non-redundant.meme
